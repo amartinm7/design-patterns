e58565383022781c2a62671f3ed148738be5f2ef
antoniomm <antonio.martinmoreno@zooplus.com>
2018-12-18 16:47:59 +0100

    updating factory pattern

--- src/main/java/org/amm/design/patterns/creational/factory/ShapeFactory.java
+++ src/main/java/org/amm/design/patterns/creational/factory/ShapeFactory.java
@@ -1,5 +1,9 @@
 package org.amm.design.patterns.creational.factory;
 
+/**
+ * create an new object without exposing the creation logic to the client and refer
+ * to newly created object using a common interface.
+ */
 public class ShapeFactory {
     //use getShape method to get object of type shape
     public Shape getShape(String shapeType){
