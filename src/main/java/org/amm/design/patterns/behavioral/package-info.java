19a361e213dcb15e9fde9fe6895fc5e89e532a22
antoniomm <antonio.martinmoreno@zooplus.com>
2018-12-18 16:39:36 +0100

    adding package-info

---
+++ src/main/java/org/amm/design/patterns/behavioral/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Behavioral Patterns
+ * These design patterns are specifically concerned with communication between objects.
+ */
+package org.amm.design.patterns.behavioral;
---
+++ src/main/java/org/amm/design/patterns/structural/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Structural Patterns
+ * These design patterns concern class and object composition.
+ * Concept of inheritance is used to compose interfaces and define ways
+ * to compose objects to obtain new functionalities.
+ */
+package org.amm.design.patterns.structural;
